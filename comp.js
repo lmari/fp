1bb6184c6840a0422bfe48ae3d223ee4ff4aeb77
Luca Mari <lmari@liuc.it>
2022-07-20 15:36:58 +0200

    update vari

--- comp.js
+++ comp.js
@@ -8,11 +8,11 @@ console.log('prima soluzione: ' + succ(double(a)))
 
 // Seconda soluzione:
 // (meta-)funzione di composizione con argomento dichiarato...
-const comp0 = (g,f,x) => g(f(x))
-console.log('seconda soluzione: ' + comp0(succ,double,a))
+const comp0 = (g, f, x) => g(f(x))
+console.log('seconda soluzione: ' + comp0(succ, double, a))
 
 // ... che dunque si può usare anche così:
-const succ_double0 = x => comp0(succ,double,x)
+const succ_double0 = x => comp0(succ, double, x)
 console.log('ancora seconda soluzione: ' + succ_double0(a))
 
 // (mentre questo...
@@ -24,5 +24,5 @@ console.log('ancora seconda soluzione: ' + succ_double0(a))
 // (meta-)funzione di composizione che restituisce una funzione...
 const comp1 = (g,f) => { return x => g(f(x)) }
 // ... che dunque si usa così:
-const succ_double1 = comp1(succ,double)
+const succ_double1 = comp1(succ, double)
 console.log('terza soluzione: ' + succ_double1(a))
